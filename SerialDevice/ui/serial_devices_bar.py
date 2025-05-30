d1bb56a7b6f249700342396d47809c9e5df5cdd7
Artwr Alpha Giga Rizzler <artwrrizzler@kuroskibidi.aba>
2025-05-30 02:07:52 -0600

    iUI

--- SerialDevice/ui/serial_devices_bar.py
+++ SerialDevice/ui/serial_devices_bar.py
@@ -3,9 +3,11 @@ from tkinter import Frame
 from tkinter.ttk import Combobox
 from tkinter import Label
 from tkinter import Text
+from tkinter import END
 
 from SerialDevice.serial_device import SerialDevice
 from SerialDevice.serial_device import BAUDRATES
+from SerialDevice.caesar import caesar_encrypt, caesar_decrypt
 
 class SerialDevicesBar(Frame):
     def __init__(self, master= None, font :str = "Arial",
@@ -33,6 +35,12 @@ class SerialDevicesBar(Frame):
                 values =BAUDRATES,
                 font=[font,fsize],
                 foreground=fcolor
+                )
+        self.caesar_combobox = Combobox(
+                self, 
+                values = ['Encrypt', 'Decrypt','None'],
+                font = [font,fsize],
+                foreground = fcolor,
                 )
         self.send_msg_label = Label(
                 self, 
@@ -67,6 +75,7 @@ class SerialDevicesBar(Frame):
                 height=2,
                 foreground=fcolor
                 )
+        self.caesar = 0;
         self.init_gui()
 
 
@@ -74,13 +83,17 @@ class SerialDevicesBar(Frame):
     def init_gui(self):
         self.serial_devices_label.pack(side='top', padx=5,pady=5,fill='x')
         self.serial_devices_combobox.pack(side='top',fill='x')
+        self.caesar_combobox.pack(side='top', padx=5,pady=5,fill='x')
         self.baudrates_combobox.pack(side='top',fill='x')
         self.send_msg_label.pack(side='top', padx=5,pady=5,fill='x')
         self.textbox.pack(side='top', padx=5,pady=5,fill='x')
         self.send_message_button.pack(side='top', padx=5,pady=5,fill='x')
         self.serial_devices_combobox.current(0)
         self.baudrates_combobox.current(0)
-        self.serial_devices_combobox.bind('<<ComboSellected>>', self.connect_arduino) 
+        self.caesar_combobox.current(2)
+        self.serial_devices_combobox.bind('<<ComboboxSelected>>',self.connect_arduino) 
+        self.baudrates_combobox.bind("<<ComboboxSelected>>",  self.connect_arduino)
+        self.caesar_combobox.bind("<<ComboboxSelected>>",  self.set_caesar)
         self.recieve_msg_label.pack(side='top', padx=5,pady=5,fill='x')
         self.textbox_recieve_msg.pack(side='top', padx=5,pady=5,fill='x')
 
@@ -90,21 +103,44 @@ class SerialDevicesBar(Frame):
         post_list.extend(SerialDevice.find_available_serial_ports())
         return port_list
 
-    def connect_arduino(self):
+    def connect_arduino(self, event):
+        print("Looking for Arduino...")
         if self.arduino is None and self.serial_devices_combobox.get() != 'Port:':
             self.arduino = SerialDevice(
                     port = self.serial_devices_combobox.get(),
                     baudrate = int(self.baudrates_combobox.get())
                 )
+            print('Arduino fonund')
         elif self.serial_devices_combobox.get() != 'Port:':
             self.arduino.diconnect()
             self.arduino = SerialDevice(
                     port = self.serial_devices_combobox.get(),
                     baudrate = int(self.baudrates_combobox.get())
                     )
+            print('Arduino fonund')
 
     def send_msg(self):
-        text = self.textbox.get("1.0") + '\n' 
+        text = self.textbox.get('1.0',"end-1c")
+        text = self.caesar_msg(text)
         if self.arduino is not None:
-            recieved=self.arduino.send_message(text_to_send)
-            self.textbox_recieve_msg.insert(0,recieved)
+            recieved=self.arduino.send_msg(text)
+            recieved = recieved.replace('\r','')
+            self.textbox_recieve_msg.insert("1.0",recieved)
+        else:
+            print("No arduino Found :(")
+
+    def set_caesar(self,event):
+        if self.caesar_combobox.get() == 'None':
+            self.caesar = 0
+        elif self.caesar_combobox.get() == 'Encrypt':
+            self.caesar = 1
+        else:
+            self.caesar = 2
+
+    def caesar_msg(self, text:str):
+        if self.caesar == 0:
+            return text
+        elif self.caesar == 1:
+            return caesar_encrypt(text, 4)
+        else:
+            return caesar_decrypt(text,4)
